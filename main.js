7b9f4adbfa4c0f83061dc9ab396df2749aa6ad82
Julia Gu <julia.gu01@gmail.com>
2024-11-15 06:53:25 -0800

    Condense scene setup

--- main.js
+++ main.js
@@ -31,42 +31,6 @@ controls.enabled = true;
 controls.minDistance = 10;
 controls.maxDistance = 50;
 
-function translationMatrix(tx, ty, tz) {
-	return new THREE.Matrix4().set(
-		1, 0, 0, tx,
-		0, 1, 0, ty,
-		0, 0, 1, tz,
-		0, 0, 0, 1
-	);
-}
-
-function rotationMatrixX(theta) {
-    return new THREE.Matrix4().set(
-        1, 0, 0, 0,
-        0, Math.cos(theta), -Math.sin(theta), 0,
-        0, Math.sin(theta), Math.cos(theta), 0,
-        0, 0, 0, 1
-    );
-}
-
-function rotationMatrixY(theta) {
-    return new THREE.Matrix4().set(
-        Math.cos(theta), 0, Math.sin(theta), 0,
-        0, 1, 0, 0,
-        -Math.sin(theta), 0, Math.cos(theta), 0,
-        0, 0, 0, 1
-    );
-}
-
-function rotationMatrixZ(theta) {
-	return new THREE.Matrix4().set(
-		Math.cos(theta), -Math.sin(theta), 0, 0,
-		Math.sin(theta),  Math.cos(theta), 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	);
-}
-
 // Create a separate scene and camera for UI elements, using an orthographic camera
 const uiScene = new THREE.Scene();
 const uiCamera = new THREE.OrthographicCamera(
@@ -75,85 +39,107 @@ const uiCamera = new THREE.OrthographicCamera(
     0.1, 10
 );
 
-// Creating objects
+// Object modeling helper functions
+function createTableWithHole(holeX, holeY) {
+    const tableShape = new THREE.Shape();
+    tableShape.setFromPoints([
+        new THREE.Vector2(-16, -31),
+        new THREE.Vector2(16, -31),
+        new THREE.Vector2(16, 31),
+        new THREE.Vector2(-16, 31)
+    ]);
 
-const ball_geometry = new THREE.SphereGeometry(1, 16, 16);
-const side_wall_geometry = new THREE.BoxGeometry(1, 800, 1200);
-const non_side_wall_geometry = new THREE.BoxGeometry(800, 800, 1);
-const side_edge_geometry = new THREE.BoxGeometry(2, 2, 64);
-const non_side_edge_geometry = new THREE.BoxGeometry(34, 2, 2);
+    const holeShape = new THREE.Shape();
+    holeShape.absellipse(holeX, holeY, 1.25, 1.25, 0, 2 * Math.PI);
+    tableShape.holes.push(holeShape);
 
-const table_points = [
-    new THREE.Vector2(-16, -31),
-    new THREE.Vector2(16, -31),
-    new THREE.Vector2(16, 31),
-    new THREE.Vector2(-16, 31)
-];
-const table_shape = new THREE.Shape();
-table_shape.setFromPoints(table_points);
-const hole_shape = new THREE.Shape();  // hole in table
-hole_shape.absellipse(0, -10, 1.25, 1.25, 0, 2 * Math.PI);
-table_shape.holes.push(hole_shape);
-const extrudeSettings = {
-    depth: 1,
-    bevelEnabled: false
-};
-const table_geometry = new THREE.ExtrudeGeometry(table_shape, extrudeSettings);
+    return new THREE.ExtrudeGeometry(tableShape, { depth: 1, bevelEnabled: false });
+}
 
-const ball_material = new THREE.MeshPhongMaterial({ color: 0xb0b0b0 });
-const table_material = new THREE.MeshPhongMaterial({color: 0x00ff00,shininess: 100});
-const wall_material = new THREE.MeshPhongMaterial({ color: 0x404040, shininess: 100 });
-const edge_material = new THREE.MeshPhongMaterial({ color: 0x806040, shininess: 100 });
+// Custom ExtrudeGeometry for box bounds
+// Credit: https://discourse.threejs.org/t/round-edged-box/1402
+function createRoundedBox(width, height, depth, radius0, smoothness) {
+    let shape = new THREE.Shape();
+    let eps = 0.00001;
+    let radius = radius0 - eps;
+    shape.absarc(eps, eps, eps, -Math.PI / 2, -Math.PI, true);
+    shape.absarc(eps, height, eps, Math.PI, Math.PI / 2, true);
+    shape.absarc(width, height, eps, Math.PI / 2, 0, true);
+    shape.absarc(width, eps, eps, 0, -Math.PI / 2, true);
+    let geometry = new THREE.ExtrudeGeometry(shape, {
+        depth: depth,
+        bevelEnabled: true,
+        bevelSegments: smoothness * 2,
+        steps: 1,
+        bevelSize: radius,
+        bevelThickness: radius0,
+        curveSegments: smoothness
+    });
+    geometry.center();
+    return geometry;
+}
 
-let ball = new THREE.Mesh(ball_geometry, ball_material);
-//ball.matrixAutoUpdate = false;
+// Manage array of box bounds
+let boxBounds = [];
+function createBoxBound(box, ball) {
+    const boundGeometry = createRoundedBox(
+        box.geometry.parameters.width,
+        box.geometry.parameters.height,
+        box.geometry.parameters.depth,
+        ball.geometry.parameters.radius,
+        1
+    );
+    let bound = new THREE.Mesh(boundGeometry, ballMaterial);
+    bound.position.copy(box.position);
+    boxBounds.push(bound);
+    scene.add(bound);
+}
+
+
+// Create objects
+const ballMaterial = new THREE.MeshPhongMaterial({ color: 0xb0b0b0 });
+const tableMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00, shininess: 100 });
+const wallMaterial = new THREE.MeshPhongMaterial({ color: 0x404040, shininess: 100 });
+const edgeMaterial = new THREE.MeshPhongMaterial({ color: 0x806040, shininess: 100 });
+
+let ball = new THREE.Mesh(new THREE.SphereGeometry(1, 16, 16), ballMaterial);
 scene.add(ball);
 
-let table = new THREE.Mesh(table_geometry, table_material);
-table.matrixAutoUpdate = false;
-table.matrix.premultiply(rotationMatrixX(Math.PI/2));
-table.matrix.premultiply(translationMatrix(0, -1, 0));
+let table = new THREE.Mesh(createTableWithHole(0, -10), tableMaterial);
+table.position.y = -1;
+table.rotateX(Math.PI/2);
 scene.add(table);
 
-let left_edge = new THREE.Mesh(side_edge_geometry, edge_material);
-left_edge.matrixAutoUpdate = false;
-left_edge.matrix.premultiply(translationMatrix(-16, -0.5, 0));
-scene.add(left_edge);
+let tableEdgeSpecs = [
+    { dims: [2, 4, 64], pos: [-16, -0.5, 0] },  // Left
+    { dims: [2, 4, 64], pos: [16, -0.5, 0] },  // Right
+    { dims: [34, 4, 2], pos: [0, -0.5, 31] },  // Far
+    { dims: [34, 4, 2], pos: [0, -0.5, -31] },  // Near
+];
+for (const spec of tableEdgeSpecs) {
+    let edge = new THREE.Mesh(new THREE.BoxGeometry(...(spec.dims)), edgeMaterial);
+    edge.position.set(...(spec.pos));
+    scene.add(edge);
+    createBoxBound(edge, ball);
+}
 
-let right_edge = new THREE.Mesh(side_edge_geometry, edge_material);
-right_edge.matrixAutoUpdate = false;
-right_edge.matrix.premultiply(translationMatrix(16, -0.5, 0));
-scene.add(right_edge);
+let wallSpecs = [
+    { dims: [1200, 800], pos: [-400, 0, 0], angle: Math.PI / 2 },  // Left
+    { dims: [1200, 800], pos: [400, 0, 0], angle: Math.PI / -2 },  // Right
+    { dims: [800, 800], pos: [0, 0, -600], angle: 0 },  // Far
+    { dims: [800, 800], pos: [0, 0, 600], angle: Math.PI },  // Near
+];
+for (const spec of wallSpecs) {
+    let wall = new THREE.Mesh(new THREE.PlaneGeometry(...(spec.dims)), wallMaterial);
+    wall.rotateY(spec.angle);
+    wall.position.set(...(spec.pos));
+    scene.add(wall);
+}
+let floor = new THREE.Mesh(new THREE.PlaneGeometry(800, 1200), wallMaterial);
+floor.rotateX(Math.PI / -2);
+floor.position.y = -400;
+scene.add(floor);
 
-let front_edge = new THREE.Mesh(non_side_edge_geometry, edge_material);
-front_edge.matrixAutoUpdate = false;
-front_edge.matrix.premultiply(translationMatrix(0, -0.5, 31));
-scene.add(front_edge);
-
-let back_edge = new THREE.Mesh(non_side_edge_geometry, edge_material);
-back_edge.matrixAutoUpdate = false;
-back_edge.matrix.premultiply(translationMatrix(0, -0.5, -31));
-scene.add(back_edge);
-
-let left_wall = new THREE.Mesh(side_wall_geometry, wall_material);
-left_wall.matrixAutoUpdate = false;
-left_wall.matrix.premultiply(translationMatrix(-400, 0, 0));
-scene.add(left_wall);
-
-let right_wall = new THREE.Mesh(side_wall_geometry, wall_material);
-right_wall.matrixAutoUpdate = false;
-right_wall.matrix.premultiply(translationMatrix(400, 0, 0));
-scene.add(right_wall);
-
-let back_wall = new THREE.Mesh(non_side_wall_geometry, wall_material);
-back_wall.matrixAutoUpdate = false;
-back_wall.matrix.premultiply(translationMatrix(0, 0, -600));
-scene.add(back_wall);
-
-let front_wall = new THREE.Mesh(non_side_wall_geometry, wall_material);
-front_wall.matrixAutoUpdate = false;
-front_wall.matrix.premultiply(translationMatrix(0, 0, 600));
-scene.add(front_wall);
 
 // Create the power bar geometry and material
 const powerBarGeometry = new THREE.PlaneGeometry(150, 20); // Width and height of the bar
@@ -216,16 +202,17 @@ let animation_time = 0;
 let delta_animation_time;
 const clock = new THREE.Clock();
 
+let pastPos = new THREE.Vector3();
+
 function animate() {
     requestAnimationFrame(animate);
-    
+
+    pastPos = ball.position;
+
     let elapsedTime = clock.getElapsedTime();
 
     delta_animation_time = clock.getDelta();
     animation_time += delta_animation_time;
-
-    let transformation = new THREE.Matrix4();
-    ball.matrix = transformation.clone();
     
     let period10 = elapsedTime % 10.0;
     let normalized_period = period10/10;
@@ -245,9 +232,45 @@ function animate() {
 
     // Update ball position based on velocity
     ball.position.add(ballVelocity);
-  
+
     // Check for floor collision
     checkFloorCollision();
+
+    /*
+    // Manual raytracing for collision detection
+    const ray = new THREE.Ray(pastPos).lookAt(ball.position);
+    let closestIntersection = null;
+    for (const bound of boxBounds) {
+        const indices = bound.geometry.getAttribute("position").array;
+        for (let i = 0; i < indices.length; i += 9) {
+            const vertices = [];
+            vertices.push(new THREE.Vector3().fromArray(indices, i + 0).add(bound.position));
+            vertices.push(new THREE.Vector3().fromArray(indices, i + 3).add(bound.position));
+            vertices.push(new THREE.Vector3().fromArray(indices, i + 6).add(bound.position));
+
+            const intersection = new THREE.Vector3();
+            if (ray.intersectTriangle(...vertices, true, intersection)) {
+                const distance = intersection.distanceTo(pastPos);
+                if (closestIntersection === null || closestIntersection.distance > distance)
+                    closestIntersection = { distance: distance, vertices: vertices };
+            }
+        }
+    }
+
+    // Reflect ball out of collision surface if necessary
+    if (closestIntersection !== null) {
+        const reflectionPlane = new THREE.Plane();
+        reflectionPlane.setFromCoplanarPoints(...(closestIntersection.vertices));
+
+        // An offset to compensate for reflecting across non-affine plane
+        const reflectionCompensation = reflectionPlane.normal.clone().setLength(-2 * reflectionPlane.constant);
+        ball.position.reflect(reflectionPlane.normal).add(reflectionCompensation);
+        ball.updateMatrixWorld();
+
+        ballVelocity.reflect(reflectionPlane.normal);
+    }
+    */
+
     controls.target.copy(ball.position);
     controls.update();
     renderer.render(scene, camera);
@@ -258,3 +281,4 @@ function animate() {
     renderer.render(uiScene, uiCamera);
 }
 animate();
+
